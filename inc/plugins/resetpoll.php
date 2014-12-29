4f86f6367f47953a68bba69b5b707634c6550bbb
PaulBender <paulbender@live.com>
2014-12-29 13:59:40 -0500

    Added codename

--- inc/plugins/resetpoll.php
+++ inc/plugins/resetpoll.php
@@ -39,6 +39,7 @@ function resetpoll_info()
 		"author"			=> "Starpaul20",
 		"authorsite"		=> "http://galaxiesrealm.com/index.php",
 		"version"			=> "1.0",
+		"codename"			=> "resetpoll",
 		"compatibility"		=> "18*"
 	);
 }
