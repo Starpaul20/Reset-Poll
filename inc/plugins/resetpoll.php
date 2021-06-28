a6ca352100a71fd3d9e16fbd09f91e570fb8bfab
Paul Bender <paulbender@live.com>
2021-06-28 16:39:12 -0400

    Bump to version v1.1

--- inc/plugins/resetpoll.php
+++ inc/plugins/resetpoll.php
@@ -41,7 +41,7 @@ function resetpoll_info()
 		"website"			=> "http://galaxiesrealm.com/index.php",
 		"author"			=> "Starpaul20",
 		"authorsite"		=> "http://galaxiesrealm.com/index.php",
-		"version"			=> "1.0.1",
+		"version"			=> "1.1",
 		"codename"			=> "resetpoll",
 		"compatibility"		=> "18*"
 	);
